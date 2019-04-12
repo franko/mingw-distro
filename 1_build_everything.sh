f70bc084d198efdfc3b3c7bd80113dac040d2890
Francesco Abbate <francesco.bbt@gmail.com>
2019-04-12 13:11:50 +0200

    Keep only tools from compiler toolchain in buind script

--- 1_build_everything.sh
+++ 1_build_everything.sh
@@ -1,29 +1,25 @@
 #!/bin/sh
 
-source ./0_append_distro_path.sh
-
 ./binutils.sh
-./boost.sh
 ./coreutils.sh
-echo SKIPPING
-cat << EOF
-./freetype.sh
-./gdb.sh
-./glbinding.sh
-./glm.sh
-./grep.sh
-./lame.sh
-./libjpeg-turbo.sh
-./libpng.sh
-EOF
 ./make.sh
 ./mingw-w64+gcc.sh
-exit 1
-./optipng.sh
-./pcre.sh
-./pcre2.sh
-./pngcheck.sh
-./sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
-./sed.sh
-./zlib.sh
-./zstd.sh
+./boost.sh
+./gdb.sh
+
+# Removed from original mingw-distro from STL
+# ./freetype.sh
+# ./glbinding.sh
+# ./glm.sh
+# ./grep.sh
+# ./lame.sh
+# ./libjpeg-turbo.sh
+# ./libpng.sh
+# ./optipng.sh
+# ./pcre.sh
+# ./pcre2.sh
+# ./pngcheck.sh
+# ./sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
+# ./sed.sh
+# ./zlib.sh
+# ./zstd.sh
